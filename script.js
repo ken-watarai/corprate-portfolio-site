b7b486b449bddfed0fc86b0d5ff8b02edf036671
ken-watarai <66110193+ken-watarai@users.noreply.github.com>
2021-06-09 22:31:15 +0900

    initial commit.
    コーポレートサイトのポートフォリオの公開、最初の投稿

---
+++ script.js
@@ -0,0 +1,13 @@
+$(window).on('scroll',function(){
+  
+  // https://yuyauver98.me/scroll-visible-range-underline-animation/
+  $(".JS_ScrollAnimationItem").each(function(){
+    var position = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > position - windowHeight){
+      $(this).addClass('isActive');
+    }
+  });
+  
+});
